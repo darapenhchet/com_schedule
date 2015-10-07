d0eca9dce9d142f97a62c870175d56c331d52f25
darapenhchet <darapenhchet@gmail.com>
2015-10-07 23:37:43 +0700

    CHANGE SQL

--- install.sql
+++ install.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `dbsc_schedule_ci`;
+﻿DROP TABLE IF EXISTS `dbsc_schedule_ci`;
 CREATE TABLE `dbsc_schedule_ci` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` text,
@@ -11,5 +11,18 @@ CREATE TABLE `dbsc_schedule_ci` (
   `imageurl` text,
   `url` text,
   `type` text,
+  `shift` text,
+  `status` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `dbsc_schedule_ci_type`;
+CREATE TABLE `dbsc_schedule_ci_type` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+INSERT INTO `dbsc_schedule_ci_type` VALUES ('1', 'ព្រឹទ្ធសភា', 'ព្រឹទ្ធសភា');
+INSERT INTO `dbsc_schedule_ci_type` VALUES ('2', 'អគ្គលេខាធិការដ្ឋាន', 'អគ្គលេខាធិការដ្ឋាន');
--- uninstall.sql
+++ uninstall.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS `dbsc_schedule_ci`;
+DROP TABLE IF EXISTS `dbsc_schedule_ci_type`;
